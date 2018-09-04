1d131fc2361ff0ec8fa446fdfa78fa32102a182f
Dmytro Lytovchenko <dmytro.lytovchenko@erlang-solutions.com>
2018-09-04 18:09:15 +0200

    Version bump, readme update

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='Pyrlang',
-      version='0.4',
-      description='Erlang Node implemented in Python 3.5/Gevent',
+      version='0.5',
+      description='Erlang Node implemented in Python 3.5/Gevent/Asyncio',
       author='Erlang Solutions Ltd and S2HC Sweden AB',
       author_email='dmytro.lytovchenko@gmail.com,pyrlang@s2hc.com',
       url='http://pyrlang.readthedocs.io/',
