12b25167914cb8060654e329b6608434d4d38e8b
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2023-04-23 15:51:08 +0200

    Version bump; Publish instructions

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 from setuptools import setup, find_packages
 
+PKGNAME = 'pyrlang'
+VERSION = '0.12-alpha'
+DESCRIPTION = 'Erlang Node implemented in Python using asyncio'
+AUTHOR = 'Erlang Solutions Ltd and S2HC Sweden AB'
+AUTHOR_EMAIL = 'dmytro.lytovchenko@gmail.com,pyrlang@s2hc.com'
+URL = 'https://github.com/Pyrlang/Pyrlang/'
 
 with open("README.md", "r", encoding='utf-8') as fp:
     LONG_DESCRIPTION = fp.read()
 
-
-setup(name='pyrlang',
-      version='0.11-beta1',
-      description='Erlang Node implemented in Python using asyncio',
+setup(name=PKGNAME,
+      version=VERSION,
+      description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       long_description_content_type="text/markdown",
-      author='Erlang Solutions Ltd and S2HC Sweden AB',
-      author_email='dmytro.lytovchenko@gmail.com,pyrlang@s2hc.com',
-      url='https://github.com/Pyrlang/Pyrlang/',
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
       packages=find_packages(),
       install_requires=["pyrlang-term>=1.2"],
       classifiers=[
-            "Programming Language :: Python :: 3.7",
-            "License :: OSI Approved :: Apache Software License"
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: Apache Software License"
       ])
